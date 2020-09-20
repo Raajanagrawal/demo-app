27e3c0d1786fd37cbef66866d4608ff05f8ee823
Raajanagrawal <Raajanagrawal@gmail.com>
2020-09-20 17:48:03 +0530

    changes for Hobbies, scroll page, contact card, Home page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,7 +8,7 @@
   "use strict";
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#header').outerHeight() - 31;
+  var scrolltoOffset = $('#header').outerHeight() - 58;
   if (window.matchMedia("(max-width: 991px)").matches) {
     scrolltoOffset += 30;
   }
